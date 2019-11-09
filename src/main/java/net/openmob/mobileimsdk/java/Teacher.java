226e88a169080022915fe15996d73d937611ce11
ckx <928050464@qq.com>
2019-11-09 17:05:32 +0800

    正式提交：thinkpad新增Teacher类

---
+++ src/main/java/net/openmob/mobileimsdk/java/Teacher.java
@@ -0,0 +1,44 @@
+package net.openmob.mobileimsdk.java;
+
+public class Teacher {
+
+    private String name;
+
+    private int age;
+
+    private int sex;
+
+    private String subject;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getSex() {
+        return sex;
+    }
+
+    public void setSex(int sex) {
+        this.sex = sex;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+}
