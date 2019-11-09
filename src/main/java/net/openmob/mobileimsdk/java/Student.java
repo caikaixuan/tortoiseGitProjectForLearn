a8ab645d20ae9829833de0b8ec17d013c45146d8
ckx <928050464@qq.com>
2019-11-09 16:32:56 +0800

    thinkpad补充提交更新后的目录结构

---
+++ src/main/java/net/openmob/mobileimsdk/java/Student.java
@@ -0,0 +1,53 @@
+package net.openmob.mobileimsdk.java;
+
+public class Student {
+    private String name;
+
+    private int age;
+
+    private int sex;
+
+    private int grade;
+
+    private int classNum;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getSex() {
+        return sex;
+    }
+
+    public void setSex(int sex) {
+        this.sex = sex;
+    }
+
+    public int getGrade() {
+        return grade;
+    }
+
+    public void setGrade(int grade) {
+        this.grade = grade;
+    }
+
+    public int getClassNum() {
+        return classNum;
+    }
+
+    public void setClassNum(int classNum) {
+        this.classNum = classNum;
+    }
+}
